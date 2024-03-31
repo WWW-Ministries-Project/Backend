8b4a4d8e1cc9d8a27b9fb0e69df4e826d21c20ba
Saah Asiedu <saah.asiedu@amalitech.org>
2024-03-31 08:31:20 +0000

    exposed assets routes

--- index.ts
+++ index.ts
@@ -12,6 +12,7 @@ import { hashPassword } from "./src/utils/hashPasswords";
 import { accessRouter } from "./src/routes/accessLevelRoute";
 import { landingPage } from "./src/controllers/userController";
 import { uploadRouter } from "./src/routes/uploadRoute";
+import { assetRouter } from "./src/routes/assetRouter";
 dotenv.config();
 
 // router
@@ -29,6 +30,7 @@ app.use("/department", departmentRouter);
 app.use("/position", positionRouter);
 app.use("/access", accessRouter);
 app.use("/upload", uploadRouter);
+app.use("/assets", assetRouter);
 
 // mongoose
 //   .connect(MONGO_URI, {})
