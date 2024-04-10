66218b277e4d3c402a9480e564b67aade5455214
Saah Asiedu <saah.asiedu@amalitech.org>
2024-04-10 15:48:05 +0000

    Stop tracking prisma/migrations folder

--- prisma/migrations/20231021194939_position/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ADD COLUMN     "position_id" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "user" ADD CONSTRAINT "user_position_id_fkey" FOREIGN KEY ("position_id") REFERENCES "position"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20231109200822_default_user_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ALTER COLUMN "is_user" SET DEFAULT false;
