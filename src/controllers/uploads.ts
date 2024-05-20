7b043cf7a77cba3292e9185a5e2e432e36bab54a
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2024-05-20 12:09:01 +0000

    hot fix

--- src/controllers/uploads.ts
+++ src/controllers/uploads.ts
@@ -1,24 +1,28 @@
 import cloudinary from "../utils/cloudinary";
 
 export const uploadImage = async (req: any, res: any) => {
-  const file = req.file.path;
-  cloudinary.uploader.upload(
-    file,
-    { folder: "www-ministires", quality: "auto" },
-    (err: any, result: any) => {
-      if (err) {
-        return res
-          .status(400)
-          .json({ message: "Error uploading file", error: err });
-      }
+  try {
+    const file = req.file.path;
+    cloudinary.uploader.upload(
+      file,
+      { folder: "www-ministires", quality: "auto" },
+      (err: any, result: any) => {
+        if (err) {
+          return res
+            .status(400)
+            .json({ message: "Error uploading file", error: err });
+        }
 
-      // File uploaded successfully to Cloudinary
-      res.status(200).json({
-        message: "File uploaded successfully",
-        result: {
-          link: result.secure_url,
-        },
-      });
-    }
-  );
+        // File uploaded successfully to Cloudinary
+        res.status(200).json({
+          message: "File uploaded successfully",
+          result: {
+            link: result.secure_url,
+          },
+        });
+      }
+    );
+  } catch (error) {
+    return res.status(404).send(error);
+  }
 };
