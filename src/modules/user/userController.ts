4eb2e9c3d9905aa13f077af18e3df25753e7b966
Clement Koomson <clement.koomson@4-sure.net>
2025-03-28 23:54:08 +0000

    hot fixes

--- src/modules/user/userController.ts
+++ src/modules/user/userController.ts
@@ -762,11 +762,54 @@ export const getUser = async (req: Request, res: Response) => {
           },
         },
         access: true,
+        enrollments: {
+          select: {
+            course: {
+              select: {
+                id: true,
+                name: true,
+                cohort: {
+                  select: {
+                    id: true,
+                    name: true,
+                    program: {
+                      select: {
+                        id: true,
+                        title: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        children: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            membership_type: true,
+            created_at: true,
+            status: true,
+            user_info: {
+              select: {
+                first_name: true,
+                last_name: true,
+                other_name: true,
+                date_of_birth: true,
+                gender: true,
+                nationality: true,
+              },
+            },
+          },
+        },
       },
     });
+
     let { user_info, ...rest } = response;
     res.status(200).json({
-      message: "Operation Succesful",
+      message: "Operation Successful",
       data: { ...rest, ...user_info },
     });
   } catch (error) {
@@ -891,20 +934,20 @@ export const statsUsers = async (req: Request, res: Response) => {
 };
 
 async function generateUserId(userData: any) {
-  let sync_id;
+  let sync_id=null;
   let is_sync = false;
   const prefix = process.env.ID_PREFIX || 'WWM-HC'; 
   const year = new Date().getFullYear();
   const paddedId = userData.id.toString().padStart(4, '0'); 
   const generatedUserId = `${prefix}-${year}000${paddedId}`;
-  try {
-    const zkResponse = await saveUserToZTeco(userData)
-    sync_id = zkResponse.sync_id
-    is_sync = true
-  }catch(error:any){
-    sync_id = null
-    is_sync = false
-  }
+  // try {
+  //   const zkResponse = await saveUserToZTeco(userData)
+  //   sync_id = zkResponse.sync_id
+  //   is_sync = true
+  // }catch(error:any){
+  //   sync_id = null
+  //   is_sync = false
+  // }
   
 
   return await updateUserAndSetUserId(userData.id, generatedUserId,sync_id,is_sync);
