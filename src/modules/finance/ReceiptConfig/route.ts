bb0136bf1f825abc51782c09fca58f90f89ca094
Akwaah <58732348+Akwaah@users.noreply.github.com>
2026-01-23 20:02:51 +0000

    Update route.ts

--- src/modules/finance/ReceiptConfig/route.ts
+++ src/modules/finance/ReceiptConfig/route.ts
@@ -4,10 +4,10 @@ import { ReceiptConfigController } from "./controller";
 const receiptConfigRouter = Router();
 const controller = new ReceiptConfigController();
 
-receiptConfigRouter.post("/", controller.create.bind(controller));
-receiptConfigRouter.get("/", controller.findAll.bind(controller));
-receiptConfigRouter.get("/:id", controller.findById.bind(controller));
-receiptConfigRouter.put("/:id", controller.update.bind(controller));
-receiptConfigRouter.delete("/:id", controller.delete.bind(controller));
+receiptConfigRouter.post("/create-receipt-config", controller.create);
+receiptConfigRouter.get("/get-receipt-configs", controller.findAll);
+receiptConfigRouter.get("/get-receipt-config-byId", controller.findById);
+receiptConfigRouter.put("/update-receipt-config", controller.update);
+receiptConfigRouter.delete("/delete-receipt-config", controller.delete);
 
 export default receiptConfigRouter;
