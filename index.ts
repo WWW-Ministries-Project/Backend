01d53f5b91c4643b19ff4ffbbdde3c403494c9f7
Akwaah <58732348+Akwaah@users.noreply.github.com>
2026-03-02 22:11:27 +0000

    Disable CORS checks for testing

--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ import logger from "./src/utils/logger-config";
 import client from "prom-client";
 import { logRequests } from "./src/middleWare/requestLogger";
 import { responseMessageEnhancer } from "./src/middleWare/responseMessageEnhancer";
-import { corsOptions } from "./src/middleWare/corsPolicy";
 import "./src/cron-jobs/hubtelPaymentReconciliationCron";
 import "./src/cron-jobs/requisitionNotificationCron";
 import "./src/cron-jobs/followUpNotificationCron";
@@ -24,8 +23,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.disable("x-powered-by");
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json({ limit: "1mb" }));
 app.use(express.json({ limit: "1mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "1mb" }));
